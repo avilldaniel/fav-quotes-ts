62719b6ec87b48b6fb3bf4116bfb63702afa506b
Daniel Villegas <avilldaniel@Daniels-MacBook-Pro.local>
2022-07-05 18:03:59 -0700

    migrated to typescript

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -27,17 +27,3 @@ const Quote = ({ quote }: Props) => {
 };
 
 export default Quote;
-
-// return (
-//   <Carousel.Item interval={10000}>
-//     <img
-//       className="d-block w-100"
-//       src="https://images.unsplash.com/photo-1516389207633-e5784a9518d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-//       alt="One"
-//       style={{ opacity: 0.3, borderRadius: "100px" }}
-//     />
-//     <Carousel.Caption style={{ textAlign: "center", height: "300px" }}>
-//       <h2>{elem}</h2>
-//     </Carousel.Caption>
-//   </Carousel.Item>
-// );
